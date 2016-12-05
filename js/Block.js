36cd6b5a86c70275a49e1db3ef785c28c8410bde
Yolo Jolo <jolosimeon@users.noreply.github.com>
2016-12-06 07:58:28 +0800

    Stacking lords

--- js/Block.js
+++ js/Block.js
@@ -3,45 +3,5 @@ function Block(x, y, z, cube) {
   this.y = y;
   this.z = z;
   this.cube = cube;
-  this.isOccupied = false;
-
-  this.getX = function() {
-    return this.x;
-  }
-
-  this.setX = function(x) {
-    this.x = x;
-  }
-
-  this.getY = function() {
-    return this.y;
-  }
-
-  this.setY = function(y) {
-    this.y = y;
-  }
-
-  this.getZ = function() {
-    return this.z;
-  }
-
-  this.setZ = function(z) {
-    this.z = z;
-  }
-
-  this.getCube = function() {
-    return this.cube;
-  }
-
-  this.setY = function(cube) {
-    this.cube = cube;
-  }
-
-  this.getOccupied = function() {
-    return this.cube;
-  }
-
-  this.setOccupied = function(isOccupied) {
-    this.isOccupied = isOccupied;
-  }
+  this.state = STATE.EMPTY;
 }
